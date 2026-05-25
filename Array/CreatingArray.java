a7cf322f81a4f566abb142b753df17c3058ed472
Harsh Verma <harshvermadev@gmail.com>
2026-05-25 12:06:10 +0530

    Array

---
+++ Array/CreatingArray.java
@@ -0,0 +1,17 @@
+package Search.Array;
+
+public class CreatingArray {
+    public static void main(String args[]) {
+        //creating array with values.
+        int[] arr1 = {1,33,22,54,78,90,102};
+
+        //creating empty array of n size.
+        int[] arr = new int[10];
+
+        //updating array with index number
+        arr[5] = 100;
+
+        System.out.println(arr1[4]);
+        System.out.println(arr[5]);
+    }
+}
