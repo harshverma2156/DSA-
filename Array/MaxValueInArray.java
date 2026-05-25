76daf4ae0a482babcf3e340ffd23d26f8717efc2
Harsh Verma <harshvermadev@gmail.com>
2026-05-25 15:14:58 +0530

    Max value

--- Array/MaxValueInArray.java
+++ Array/MaxValueInArray.java
@@ -1,12 +1,12 @@
 package Search.Array;
 
 public class MaxValueInArray {
-    private static void main(String[] args) {
+    static void main(String[] args) {
         int[] arr = {4,6,2,9,-112};
         int i;
         int max = arr[0];
 
-        for(i=0; i< arr.length; i++){
+        for(i=1; i< arr.length; i++){
             if(arr[i]>max){
                 max = arr[i];
             }
